46b5f26833b9901a1c3b5bff565b26c49f400aca
vaaishalijain <vaishalij00@gmail.com>
2020-08-09 18:04:07 +0530

    Path Sum III

---
+++ August-Leetcode-Challenge-Solutions/8_Path_Sum_III.py
@@ -0,0 +1,56 @@
+"""
+    Path Sum III
+
+    Q. You are given a binary tree in which each node contains an integer value.
+
+        Find the number of paths that sum to a given value.
+
+        The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from
+        parent nodes to child nodes).
+
+        The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+        Example:
+
+        root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+              10
+             /  \
+            5   -3
+           / \    \
+          3   2   11
+         / \   \
+        3  -2   1
+
+        Return 3. The paths that sum to 8 are:
+
+        1.  5 -> 3
+        2.  5 -> 2 -> 1
+        3. -3 -> 11
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        if not root:
+            return 0
+        def helper(root,cur,hash):
+            if not root:
+                return
+            cur+=root.val
+            old=cur-sum
+            self.cnt+=hash.get(old,0)
+            hash[cur]=1+hash.get(cur,0)
+            helper(root.left,cur,hash)
+            helper(root.right,cur,hash)
+            hash[cur]-=1
+        self.cnt=0
+        helper(root,0,{0:1})
+        return self.cnt
