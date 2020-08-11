7d6330073005a22593a09f67f1502b252cd80a5e
vaaishalijain <vaishalij00@gmail.com>
2020-08-11 21:36:27 +0530

    H-Index

---
+++ August-Leetcode-Challenge-Solutions/11_H-Index.py
@@ -0,0 +1,33 @@
+"""
+    H-Index
+
+    Q. Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to
+       compute the researcher's h-index.
+
+        According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at
+        least h citations each, and the other N − h papers have no more than h citations each."
+
+        Example:
+
+        Input: citations = [3,0,6,1,5]
+        Output: 3
+        Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had
+                     received 3, 0, 6, 1, 5 citations respectively.
+                     Since the researcher has 3 papers with at least 3 citations each and the remaining
+                     two with no more than 3 citations each, her h-index is 3.
+        Note: If there are several possible values for h, the maximum one is taken as the h-index.
+
+"""
+
+
+class Solution:
+    def hIndex(self, c: List[int]) -> int:
+        if not c:
+            return 0
+        c.sort()
+        n = len(c)
+        for i in range(n):
+            if c[i] >= n - i:
+                return n - i
+        return 0
+
