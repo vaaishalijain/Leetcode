f9a896bcc389646170ea4c4ec6ead3b4e178106e
vaaishalijain <vaishalij00@gmail.com>
2020-08-15 11:15:05 +0530

    Longest Palindrome

---
+++ August-Leetcode-Challenge-Solutions/14_Longest_Palindrome.py
@@ -0,0 +1,30 @@
+"""
+    Longest Palindrome
+
+    Q. Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that
+       can be built with those letters.
+
+        This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+        Note:
+        Assume the length of given string will not exceed 1,010.
+
+        Example:
+
+        Input:
+        "abccccdd"
+
+        Output:
+        7
+
+        Explanation:
+        One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans=0
+        ods=sum([i%2 for _,i in Counter(s).items()])
+        return len(s) if ods<=1 else len(s)-ods+1
